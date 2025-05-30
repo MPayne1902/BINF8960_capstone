b8abe687cb68ade358de35917b490898dce75e34
Madison Payne <Madison.Payne@uga.edu>
2025-05-30 16:22:48 -0400

    Raw and trimmed read count

--- summary_stats.sh
+++ summary_stats.sh
@@ -1,12 +1,12 @@
 # Summary of Fastq data
 
-# Count reads in raw and trimmed data
-for raw in data/raw_fastq/*.fastq
+# Count reads in raw data
+for fastq in data/*_fastq/*.fastq
 do
-	sample=$(basename $raw .fastq)
-	raw_lines=$(wc -l < data/raw_fastq/${sample}.fastq)
-	raw_reads=$(echo "$raw_lines / 4" | bc)
-	echo "$sample result: $raw_reads"
+	sample=$(basename $fastq .fastq)
+	lines=$(wc -l < data/*_fastq/${sample}.fastq)
+	reads=$(echo "$lines / 4" | bc)
+	echo "$sample result: $reads"
 done
 # Count reads that aligned to the genome
     ##Will need to use the "samtools view" command with the "-F 0x4" argument. Explain what this argument does
