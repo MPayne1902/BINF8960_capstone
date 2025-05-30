97ecc42bd75ddfa04436855c9aff7821e4f97beb
Madison Payne <Madison.Payne@uga.edu>
2025-05-29 22:35:07 -0400

    Added quality control for trimmed fastq and fixed trimming section

--- capstone.sh
+++ capstone.sh
@@ -16,12 +16,12 @@
 #cp -r data/raw_fastq/* data/backup
 
 # Load Modules
-module load FastQC/0.11.9-Java-11
+#module load FastQC/0.11.9-Java-11
 module load MultiQC/1.14-foss-2022a
-module load Trimmomatic/0.39-Java-13
-module load BWA/0.7.18-GCCcore-13.3.0 #Load BWA
-module load SAMtools/1.18-GCC-12.3.0
-module load BCFtools/1.18-GCC-12.3.0
+#module load Trimmomatic/0.39-Java-13
+#module load BWA/0.7.18-GCCcore-13.3.0 #Load BWA
+#module load SAMtools/1.18-GCC-12.3.0
+#module load BCFtools/1.18-GCC-12.3.0
 
 # Quality control on raw reads
 
@@ -32,14 +32,25 @@ module load BCFtools/1.18-GCC-12.3.0
 #multiqc -o results/fastqc_raw_results results/fastqc_raw_results
 
 # Trim raw reads with Trimmomatic
-TRIMMOMATIC="java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar"
+#gunzip data/raw_fastq/*fastq.gz
 
-for fwd in data/raw_fastq/*_1.fastq*
-do
-       echo "Trimming $sample"
-       sample=$(basename $fwd _1.fastq.gz)
-       $TRIMMOMATIC PE data/raw_fastq/${sample}_1.fastq.gz data/raw_fastq/${sample}_2.fastq.gz \
-              data/trimmed_fastq/${sample}_1.paired.fastq.gz data/trimmed_fastq/${sample}_1.unpaired.fastq.gz \
-              data/raw_fastq/${sample}_2.paired.fastq.gz data/raw_fastq/${sample}_2.unpaired.fastq.gz \
-              ILLUMINACLIP:data/raw_fastq/NexteraPE-PE.fa:2:30:10:5:True SLIDINGWINDOW:4:20
-done
+#TRIMMOMATIC="java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar"
+
+#for fwd in data/raw_fastq/*_1.fastq*
+#do
+#       sample=$(basename $fwd _1.fastq)
+#       echo "Trimming $sample"
+#       $TRIMMOMATIC PE data/raw_fastq/${sample}_1.fastq data/raw_fastq/${sample}_2.fastq \
+#              data/trimmed_fastq/${sample}_1.paired.fastq data/trimmed_fastq/${sample}_1.unpaired.fastq \
+#              data/trimmed_fastq/${sample}_2.paired.fastq data/trimmed_fastq/${sample}_2.unpaired.fastq \
+#              ILLUMINACLIP:data/raw_fastq/NexteraPE-PE.fa:2:30:10:5:True SLIDINGWINDOW:4:20
+#done
+
+# Quality control on trimmed reads
+
+## Run FastQC on trimmed reads
+#fastqc -o results/fastqc_trimmed_results data/trimmed_fastq/*.fastq*
+
+## Run MultiQC to compile the trimmed FastQC results
+multiqc -o results/fastqc_trimmed_results results/fastqc_trimmed_results
+
