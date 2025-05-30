8375f69ab7566aeca9d1e32c6dc7eeddf1304ab4
Madison Payne <Madison.Payne@uga.edu>
2025-05-29 21:40:57 -0400

    Added Trimming section to trim fastq reads

--- capstone.sh
+++ capstone.sh
@@ -29,4 +29,17 @@ module load BCFtools/1.18-GCC-12.3.0
 #fastqc -o results/fastqc_raw_results data/raw_fastq/*.fastq*
 
 ## Run MultiQC to compile the FastQC results
-multiqc -o results/fastqc_raw_results results/fastqc_raw_results
+#multiqc -o results/fastqc_raw_results results/fastqc_raw_results
+
+# Trim raw reads with Trimmomatic
+TRIMMOMATIC="java -jar $EBROOTTRIMMOMATIC/trimmomatic-0.39.jar"
+
+for fwd in data/raw_fastq/*_1.fastq*
+do
+       echo "Trimming $sample"
+       sample=$(basename $fwd _1.fastq.gz)
+       $TRIMMOMATIC PE data/raw_fastq/${sample}_1.fastq.gz data/raw_fastq/${sample}_2.fastq.gz \
+              data/trimmed_fastq/${sample}_1.paired.fastq.gz data/trimmed_fastq/${sample}_1.unpaired.fastq.gz \
+              data/raw_fastq/${sample}_2.paired.fastq.gz data/raw_fastq/${sample}_2.unpaired.fastq.gz \
+              ILLUMINACLIP:data/raw_fastq/NexteraPE-PE.fa:2:30:10:5:True SLIDINGWINDOW:4:20
+done
