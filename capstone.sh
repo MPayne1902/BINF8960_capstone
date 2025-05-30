6a19049b1d5ffb9d5046d6dabf4bf9a248bb20c8
Madison Payne <Madison.Payne@uga.edu>
2025-05-29 21:23:01 -0400

    Added modules and quality control

--- capstone.sh
+++ capstone.sh
@@ -13,4 +13,20 @@
 #chmod -w data/raw_fastq/*.fastq*
 
 ## Make backup data
-cp -r data/raw_fastq/* data/backup
+#cp -r data/raw_fastq/* data/backup
+
+# Load Modules
+module load FastQC/0.11.9-Java-11
+module load MultiQC/1.14-foss-2022a
+module load Trimmomatic/0.39-Java-13
+module load BWA/0.7.18-GCCcore-13.3.0 #Load BWA
+module load SAMtools/1.18-GCC-12.3.0
+module load BCFtools/1.18-GCC-12.3.0
+
+# Quality control on raw reads
+
+## Run FastQC on raw reads
+#fastqc -o results/fastqc_raw_results data/raw_fastq/*.fastq*
+
+## Run MultiQC to compile the FastQC results
+multiqc -o results/fastqc_raw_results results/fastqc_raw_results
