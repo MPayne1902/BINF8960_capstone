e9c51812f884ccb4baac7213f76977652c71fa98
Madison Payne <Madison.Payne@uga.edu>
2025-05-29 22:39:48 -0400

    Download E. coli genome and index it

--- capstone.sh
+++ capstone.sh
@@ -17,9 +17,9 @@
 
 # Load Modules
 #module load FastQC/0.11.9-Java-11
-module load MultiQC/1.14-foss-2022a
+#module load MultiQC/1.14-foss-2022a
 #module load Trimmomatic/0.39-Java-13
-#module load BWA/0.7.18-GCCcore-13.3.0 #Load BWA
+module load BWA/0.7.18-GCCcore-13.3.0 #Load BWA
 #module load SAMtools/1.18-GCC-12.3.0
 #module load BCFtools/1.18-GCC-12.3.0
 
@@ -52,5 +52,17 @@ module load MultiQC/1.14-foss-2022a
 #fastqc -o results/fastqc_trimmed_results data/trimmed_fastq/*.fastq*
 
 ## Run MultiQC to compile the trimmed FastQC results
-multiqc -o results/fastqc_trimmed_results results/fastqc_trimmed_results
+#multiqc -o results/fastqc_trimmed_results results/fastqc_trimmed_results
+
+# Download E. coli genome for alignment
+
+## Location to download the genome from
+genome_url="ftp://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/000/017/985/GCA_000017985.1_ASM1798v1/GCA_000017985.1_ASM1798v1_genomic.fna.gz"
+
+## Download E. coli genomes
+wget -O data/genomes/ecoli_rel606.fna.gz $genome_url
+gunzip data/genomes/ecoli_rel606.fna.gz
+
+## Index the genome
+bwa index data/genomes/ecoli_rel606.fna
 
