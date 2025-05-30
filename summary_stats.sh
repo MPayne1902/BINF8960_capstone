d61ca4e720e4691693ee5031260268f9bc7b8701
Madison Payne <Madison.Payne@uga.edu>
2025-05-30 17:46:21 -0400

    Script for counting aligned reads added

--- summary_stats.sh
+++ summary_stats.sh
@@ -1,14 +1,23 @@
 # Summary of Fastq data
 
-# Count reads in raw data
-for fastq in data/*_fastq/*.fastq
-do
-	sample=$(basename $fastq .fastq)
-	lines=$(wc -l < data/*_fastq/${sample}.fastq)
-	reads=$(echo "$lines / 4" | bc)
-	echo "$sample result: $reads"
-done
+# Count reads in raw and trimmed data
+#for fastq in data/*_fastq/*.fastq
+#do
+#	sample=$(basename $fastq .fastq)
+#	lines=$(wc -l < data/*_fastq/${sample}.fastq)
+#	reads=$(echo "$lines / 4" | bc)
+#	echo "$sample result: $reads"
+#done
+
 # Count reads that aligned to the genome
+module load SAMtools/1.18-GCC-12.3.0
+
+for aligned in results/sam/*.sam
+do
+	var=$(basename $aligned .sam)
+	count=$(samtools view -c -F 0x4 results/sam/$var.sam) #"-c" gives the count and "-F" is to exclude any flags stated, such as "0x4" which is for unmapped segments
+	echo "$var aligned reads: $count"
+done
     ##Will need to use the "samtools view" command with the "-F 0x4" argument. Explain what this argument does
 # Number of variant sites from each sample
 # Save to a tidy-formatted data file
