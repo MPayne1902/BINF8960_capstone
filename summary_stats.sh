26aec1ba837222dd55ec5ebe7f9fba12ddeeedab
Madison Payne <Madison.Payne@uga.edu>
2025-06-03 13:41:14 -0400

    Added informative comments

--- summary_stats.sh
+++ summary_stats.sh
@@ -7,10 +7,10 @@ echo "Sample,Read_Count" > summary_results/raw_read_count.csv # Make summary fil
 for raw in data/raw_fastq/*.fastq
 do
         rawsam=$(basename $raw .fastq)
-        rawline=$(wc -l < data/raw_fastq/${rawsam}.fastq)
-        rawread=$(echo "$rawline / 4" | bc)
+        rawline=$(wc -l < data/raw_fastq/${rawsam}.fastq) # Counts lines in raw data file
+        rawread=$(echo "$rawline / 4" | bc) # Divides number of lines by 4 (4 lines per read) to get the read count
         echo "$rawsam result: $rawread"
-        echo "$rawsam,$rawread" >> summary_results/raw_read_count.csv
+        echo "$rawsam,$rawread" >> summary_results/raw_read_count.csv # Saves count to summary file
 done
 
 # Count reads in trimmed data
@@ -19,10 +19,10 @@ echo "Sample,Read_Count" > summary_results/trimmed_read_count.csv # Make summary
 for trim in data/trimmed_fastq/*.fastq
 do
 	trimsam=$(basename $trim .fastq)
-	trimline=$(wc -l < data/trimmed_fastq/${trimsam}.fastq)
-	trimread=$(echo "$trimline / 4" | bc)
+	trimline=$(wc -l < data/trimmed_fastq/${trimsam}.fastq) # Counts lines in trimmed data file
+	trimread=$(echo "$trimline / 4" | bc) # Divides number of lines by 4 (4 lines per read) to get the read count
 	echo "$trimsam result: $trimread"
-	echo "$trimsam,$trimread" >> summary_results/trimmed_read_count.csv
+	echo "$trimsam,$trimread" >> summary_results/trimmed_read_count.csv # Saves count to summary file
 done
 
 # Count reads that aligned to the genome
@@ -35,7 +35,7 @@ do
 	var=$(basename $aligned .sam)
 	count=$(samtools view -c -F 0x4 results/sam/$var.sam) #"-c" gives the count and "-F" is to exclude any flags stated, such as "0x4" which is for unmapped segments
 	echo "$var aligned reads: $count"
-	echo "$var,$count" >> summary_results/aligned_read_count.csv
+	echo "$var,$count" >> summary_results/aligned_read_count.csv # Saves count to summary file
 done
 
 # Number of variant sites from each sample
@@ -44,7 +44,7 @@ echo "Sample,Variant_Sites" > summary_results/variant_site_count.csv # Make a va
 for spot in results/vcf/*.vcf
 do
 	three=$(basename $spot .vcf)
-	site=$(tail -n +29 results/vcf/${three}.vcf | wc -l)
+	site=$(tail -n +29 results/vcf/${three}.vcf | wc -l) # Counts lines (variants) after the headers (first 28 lines)
 	echo "$three variant sites: $site"
-	echo "$three,$site" >> summary_results/variant_site_count.csv
+	echo "$three,$site" >> summary_results/variant_site_count.csv # Saves count to summary file
 done
