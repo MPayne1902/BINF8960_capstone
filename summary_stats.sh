2d51c03e1890b7d7034832139dd9d48408c24398
Madison Payne <Madison.Payne@uga.edu>
2025-06-03 12:32:34 -0400

    added variant summary

--- summary_stats.sh
+++ summary_stats.sh
@@ -10,14 +10,20 @@
 #done
 
 # Count reads that aligned to the genome
-module load SAMtools/1.18-GCC-12.3.0
+#module load SAMtools/1.18-GCC-12.3.0
 
-for aligned in results/sam/*.sam
-do
-	var=$(basename $aligned .sam)
-	count=$(samtools view -c -F 0x4 results/sam/$var.sam) #"-c" gives the count and "-F" is to exclude any flags stated, such as "0x4" which is for unmapped segments
-	echo "$var aligned reads: $count"
-done
-    ##Will need to use the "samtools view" command with the "-F 0x4" argument. Explain what this argument does
+#for aligned in results/sam/*.sam
+#do
+#	var=$(basename $aligned .sam)
+#	count=$(samtools view -c -F 0x4 results/sam/$var.sam) #"-c" gives the count and "-F" is to exclude any flags stated, such as "0x4" which is for unmapped segments
+#	echo "$var aligned reads: $count"
+#done
 # Number of variant sites from each sample
+for spot in results/vcf/*.vcf
+do
+	three=$(basename $spot .vcf)
+	site=$(tail -n +29 results/vcf/${three}.vcf | wc -l)
+	echo "$three variant sites: $site"
+done
+
 # Save to a tidy-formatted data file
