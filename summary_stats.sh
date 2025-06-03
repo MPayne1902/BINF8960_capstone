3d417cc02a45dc3ee8353ff6ac9a87ad8e488d36
Madison Payne <Madison.Payne@uga.edu>
2025-06-03 13:34:00 -0400

    Make separate separate summary files for each category (read count, aligned reads, variant count, etc.

--- summary_stats.sh
+++ summary_stats.sh
@@ -1,29 +1,50 @@
 # Summary of Fastq data
+mkdir summary_results
 
-# Count reads in raw and trimmed data
-#for fastq in data/*_fastq/*.fastq
-#do
-#	sample=$(basename $fastq .fastq)
-#	lines=$(wc -l < data/*_fastq/${sample}.fastq)
-#	reads=$(echo "$lines / 4" | bc)
-#	echo "$sample result: $reads"
-#done
+# Count reads in raw data
+echo "Count Raw Reads"
+echo "Sample,Read_Count" > summary_results/raw_read_count.csv # Make summary file for raw reads
+for raw in data/raw_fastq/*.fastq
+do
+        rawsam=$(basename $raw .fastq)
+        rawline=$(wc -l < data/raw_fastq/${rawsam}.fastq)
+        rawread=$(echo "$rawline / 4" | bc)
+        echo "$rawsam result: $rawread"
+        echo "$rawsam,$rawread" >> summary_results/raw_read_count.csv
+done
+
+# Count reads in trimmed data
+echo "Count Trimmed Reads"
+echo "Sample,Read_Count" > summary_results/trimmed_read_count.csv # Make summary file for trimmed reads
+for trim in data/trimmed_fastq/*.fastq
+do
+	trimsam=$(basename $trim .fastq)
+	trimline=$(wc -l < data/trimmed_fastq/${trimsam}.fastq)
+	trimread=$(echo "$trimline / 4" | bc)
+	echo "$trimsam result: $trimread"
+	echo "$trimsam,$trimread" >> summary_results/trimmed_read_count.csv
+done
 
 # Count reads that aligned to the genome
-#module load SAMtools/1.18-GCC-12.3.0
+module load SAMtools/1.18-GCC-12.3.0
 
-#for aligned in results/sam/*.sam
-#do
-#	var=$(basename $aligned .sam)
-#	count=$(samtools view -c -F 0x4 results/sam/$var.sam) #"-c" gives the count and "-F" is to exclude any flags stated, such as "0x4" which is for unmapped segments
-#	echo "$var aligned reads: $count"
-#done
+echo "Count Aligned Reads"
+echo "Sample,Aligned_Reads" > summary_results/aligned_read_count.csv # Make a summary file for aligned reads
+for aligned in results/sam/*.sam
+do
+	var=$(basename $aligned .sam)
+	count=$(samtools view -c -F 0x4 results/sam/$var.sam) #"-c" gives the count and "-F" is to exclude any flags stated, such as "0x4" which is for unmapped segments
+	echo "$var aligned reads: $count"
+	echo "$var,$count" >> summary_results/aligned_read_count.csv
+done
+
 # Number of variant sites from each sample
+echo "Count Number of Variant Sites"
+echo "Sample,Variant_Sites" > summary_results/variant_site_count.csv # Make a variant count summary file
 for spot in results/vcf/*.vcf
 do
 	three=$(basename $spot .vcf)
 	site=$(tail -n +29 results/vcf/${three}.vcf | wc -l)
 	echo "$three variant sites: $site"
+	echo "$three,$site" >> summary_results/variant_site_count.csv
 done
-
-# Save to a tidy-formatted data file
